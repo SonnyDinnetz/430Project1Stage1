442f4d57a3a304d8862d5181b15102a73884cd76
SonnyDinnetz <Sonny.Dinnetz@gmail.com>
2025-09-28 14:26:32 -0500

    Core product members

--- Product.java
+++ Product.java
@@ -1,3 +1,26 @@
 public class Product {
+    private static int next_uid;
+    private static int get_next_uid() {return next_uid++;}
 
+    private int uid;
+    public int get_uid() {return this.uid;}
+    
+    private String name;
+    public String get_name() {return this.name;}
+    public void set_name(String name) {this.name = name;}
+
+    private int qty;
+    public int get_address() {return this.qty;}
+    public void set_address(int qty) {this.qty = qty;}
+
+    private int price;
+    public int get_balance() {return this.price;}
+    public void set_balance(int price) {this.price = price;}
+
+    public Product(String name, int qty, int price) {
+        this.uid = get_next_uid();
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
 }
